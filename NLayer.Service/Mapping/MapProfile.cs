f6ad777c35074f52f30dc1493b578827b525df22
BBerkinT <topaloglubatinberkin@gmail.com>
2023-08-30 01:45:30 +0300

    AutoMapper

---
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile() { 
+            CreateMap<Product,ProductDto>().ReverseMap();
+            CreateMap<Category,CategoryDto>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
+
+        }
+
+
+    }
+}
