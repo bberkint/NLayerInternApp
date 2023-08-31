fcaac96869a7ba646559a85287dcc50fac837c95
BBerkinT <topaloglubatinberkin@gmail.com>
2023-08-31 23:25:03 +0300

    ProductController

---
+++ NLayer.API/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+        public ProductsController(IService<Product> service, IMapper mapper) { 
+            _mapper = mapper;
+            _service = service; 
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var products = await _service.GetAllAsync();
+
+            var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList());
+            return Ok(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));
+        }
+    }
+}
