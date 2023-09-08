b67a009712c610d706f2ac04e3e99eb2907fcfc4
BBerkinT <topaloglubatinberkin@gmail.com>
2023-09-08 14:24:40 +0300

    IPrductService Update

--- NLayer.API/Controllers/ProductsController.cs
+++ NLayer.API/Controllers/ProductsController.cs
@@ -12,20 +12,18 @@ namespace NLayer.API.Controllers
     public class ProductsController : CustomBaseController
     {
         private readonly IMapper _mapper;
-        private readonly IService<Product> _service;
-        private readonly IProductService _productService;
+        private readonly IProductService _service;
         public ProductsController(IService<Product> service, IMapper mapper, IProductService productService)
         {
             _mapper = mapper;
-            _service = service;
-            _productService = productService;
+            _service = productService;
         }
 
 
         //GET api/products/GetProductsWithCategory
         [HttpGet("[action]")]
         public async Task<IActionResult> GetProductsWithCategory() {
-            return CreateActionResult(await _productService.GetProductsWithCategory());
+            return CreateActionResult(await _service.GetProductsWithCategory());
         }
 
 
--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -25,6 +25,9 @@ builder.Services.AddAutoMapper(typeof(MapProfile));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductService, ProductService>();
 
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
 
 //builder.Services.AddDbContext<AppDbContext>(options =>
 
